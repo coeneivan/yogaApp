c37436fe9aaa354f3d143a302aeac04be7da79d1
Ivan Coene <ivan@ivancoene.be>
2024-06-16 23:40:14 +0200

    chore: Update Apollo client URI to use production URL

--- fe/src/lib/apolloClient.ts
+++ fe/src/lib/apolloClient.ts
@@ -7,7 +7,7 @@ import {
 export function makeClient() {
   const httpLink = new HttpLink({
     // this needs to be an absolute url, as relative urls cannot be used in SSR
-    uri: "http://localhost:4000/",
+    uri: "https://yogaapp-1.onrender.com/",
     // you can disable result caching here if you want to
     // (this does not work if you are rendering your page with `export const dynamic = "force-static"`)
     fetchOptions: { cache: "no-store" },
